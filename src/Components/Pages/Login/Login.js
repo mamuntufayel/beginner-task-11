9e37ad6ad0ee2ccdf7c6c629d33d32261e0ff136
mamuntufayel <mamuntuffa@gmail.com>
2022-05-04 12:59:05 -0400

    social login added

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import google from "../../../images/social/google.png";
 
 const Login = () => {
   return (
@@ -9,19 +10,29 @@ const Login = () => {
       <Form className="w-50 mx-auto">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Login
         </Button>
         <p>
-          Don't have an account{" "}
+          Don't have an account?{" "}
           <span>
             {" "}
             <Link className="text-decoration-none" to="/register">
@@ -29,6 +40,15 @@ const Login = () => {
             </Link>{" "}
           </span>
         </p>
+        <div>
+          <button className="btn btn-primary d-block mx-auto w-50">
+            <span>
+              {" "}
+              <img src={google} alt="" />{" "}
+            </span>{" "}
+            Sign Up With Google
+          </button>
+        </div>
       </Form>
     </div>
   );
--- src/Components/Pages/Register/Register.js
+++ src/Components/Pages/Register/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import google from "../../../images/social/google.png";
 
 const Register = () => {
   return (
@@ -9,19 +10,39 @@ const Register = () => {
       <Form className="w-50 mx-auto">
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required />
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            autoComplete="off"
+            required
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required />
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm-Password"
+            required
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Register
         </Button>
         <p>
-          Don't have an account{" "}
+          Already registered?{" "}
           <span>
             {" "}
             <Link className="text-decoration-none" to="/login">
@@ -29,6 +50,15 @@ const Register = () => {
             </Link>{" "}
           </span>
         </p>
+        <div>
+          <button className="btn btn-primary d-block mx-auto w-50">
+            <span>
+              {" "}
+              <img src={google} alt="" />{" "}
+            </span>{" "}
+            Sign Up With Google
+          </button>
+        </div>
       </Form>
     </div>
   );
