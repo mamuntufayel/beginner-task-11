7b6a6538a27e55d94dfce35908b4900562b950df
mamuntufayel <mamuntuffa@gmail.com>
2022-05-08 06:49:27 -0400

    responsive done

--- src/Components/Pages/ProductDetail/ProductDetail.js
+++ src/Components/Pages/ProductDetail/ProductDetail.js
@@ -34,34 +34,33 @@ const ProductDetail = () => {
   };
 
   return (
-    <div className="my-4 row">
-      <Card
-        className="col-12 col-lg-12 mx-auto border border-success border border-2"
-        style={{ width: "38rem" }}
-      >
-        <Card.Img variant="top" src={productId.img} />
-        <Card.Body className="text-center">
-          <Card.Title>{productId.name}</Card.Title>
-          <Card.Text>Price:{productId.price}</Card.Text>
-          <Card.Text>Description:{productId.description}</Card.Text>
-          <Card.Text>Supplier: {productId.supplier}</Card.Text>
-          <Card.Text>Quantity(pcs): {update}</Card.Text>
-          <Button className="mb-3" onClick={handleUpdate} variant="info">
-            Delivered
-          </Button>
-          <br />
-          <input
-            onBlur={handleToRestockOnBlur}
-            type="number"
-            name="restockNumbers"
-            id=""
-          />
+    <div>
+      <div className="my-4 row">
+        <Card className="col-12 col-lg-12 mx-auto border border-success border border-2">
+          <Card.Img variant="top" src={productId.img} />
+          <Card.Body className="text-center">
+            <Card.Title>{productId.name}</Card.Title>
+            <Card.Text>Price:{productId.price}</Card.Text>
+            <Card.Text>Description:{productId.description}</Card.Text>
+            <Card.Text>Supplier: {productId.supplier}</Card.Text>
+            <Card.Text>Quantity(pcs): {update}</Card.Text>
+            <Button className="mb-3" onClick={handleUpdate} variant="info">
+              Delivered
+            </Button>
+            <br />
+            <input
+              onBlur={handleToRestockOnBlur}
+              type="number"
+              name="restockNumbers"
+              id=""
+            />
 
-          <Button className="ms-3" onClick={handleToRestock} variant="info">
-            Restock
-          </Button>
-        </Card.Body>
-      </Card>
+            <Button className="ms-3" onClick={handleToRestock} variant="info">
+              Restock
+            </Button>
+          </Card.Body>
+        </Card>
+      </div>
     </div>
   );
 };
